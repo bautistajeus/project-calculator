4e29b0dc4af28826f8049f5c2fc3b44946737759
Jeus Bautista <bautista.jeus@gmail.com>
2023-09-27 17:44:56 +0800

    Enable display box and operator display
    Mathematical operation still missing

--- script.js
+++ script.js
@@ -17,3 +17,47 @@ function calculate(operator) {
             break;
     }
 }
+
+
+
+const displayCurrent = document.querySelector('.displayCurrent')
+const displayPrevious = document.querySelector('.displayPrevious')
+const clearButton = document.querySelector('#clear')
+const buttonNumber = document.querySelectorAll('#num')
+const operatorButton = document.querySelectorAll('#operator')
+const equalsButton = document.querySelector('#equals')
+
+let firstNum = null
+let secondNum = null
+
+
+buttonNumber.forEach((button) => {
+    button.addEventListener('click', () => {
+        numbers = button.value
+        displayCurrent.textContent += numbers
+        secondNum = displayCurrent.textContent
+        console.log(secondNum)
+    })
+});
+
+operatorButton.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        displayPrevious.textContent = displayCurrent.textContent + operator.value
+        firstNum = displayCurrent.textContent
+        displayCurrent.textContent = 0
+        console.log(firstNum)
+        
+
+    })
+})
+
+equalsButton.addEventListener('click', () => {
+    
+})
+
+clearButton.addEventListener('click', () => {
+    displayCurrent.textContent = 0
+    displayPrevious.textContent = null
+})
+
+// operatorButton.addEventListener('click', calculate(operator))
