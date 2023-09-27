6e7e919dd0c7a73036dd5e5896b369c3ab6b13d9
Jeus Bautista <bautista.jeus@gmail.com>
2023-09-27 20:24:55 +0800

    Add the function of operators
    still lacking design and colors

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const a = null
-const b = null
+// const a = null
+// const b = null
 
-function calculate(operator) {
-    switch(operator) {
-        case '+':
-            return (a + b);
-            break;
-        case '-':
-            return (a - b);
-            break;
-        case '*':
-            return (a * b);
-            break;
-        case '/':
-            return (a / b);
-            break;
-    }
-}
+// function calculate(operator) {
+//     switch(operator) {
+//         case '+':
+//             return (a + b);
+//             break;
+//         case '-':
+//             return (a - b);
+//             break;
+//         case '*':
+//             return (a * b);:
+//             break;
+//         case '/':
+//             return (a / b);
+//             break;
+//     }
+// }
 
 
 
@@ -27,33 +27,63 @@ const buttonNumber = document.querySelectorAll('#num')
 const operatorButton = document.querySelectorAll('#operator')
 const equalsButton = document.querySelector('#equals')
 
-let firstNum = null
-let secondNum = null
 
 
-buttonNumber.forEach((button) => {
-    button.addEventListener('click', () => {
-        numbers = button.value
-        displayCurrent.textContent += numbers
-        secondNum = displayCurrent.textContent
-        console.log(secondNum)
+function calculate() {
+    buttonNumber.forEach((button) => {
+        button.addEventListener('click', () => {
+            numbers = button.value
+            displayCurrent.textContent += numbers
+            
+            
+        })
+    });
+    var number_array = []
+    var operator_array = []
+    operatorButton.forEach((operator) => {
+        operator.addEventListener('click', () => {
+            displayPrevious.textContent += displayCurrent.textContent + operator.value
+            value = displayCurrent.textContent
+            displayCurrent.textContent = null
+            operatorChoice = operator.value
+            number_array.push(value)
+            number_array.push(operatorChoice)
+                if(operatorChoice == '=') {
+                    number_array.pop()
+                    displayCurrent.textContent = eval(number_array.join(''))
+                    displayPrevious.textContent = 0
+                    number_array = []
+                }
+                
+        })
     })
-});
+    
+        
+}
+calculate()
+// buttonNumber.forEach((button) => {
+//     button.addEventListener('click', () => {
+//         numbers = button.value
+//         displayCurrent.textContent += numbers
+//         secondNum = displayCurrent.textContent
+//         console.log(secondNum)
+//     })
+// });
 
-operatorButton.forEach((operator) => {
-    operator.addEventListener('click', () => {
-        displayPrevious.textContent = displayCurrent.textContent + operator.value
-        firstNum = displayCurrent.textContent
-        displayCurrent.textContent = 0
-        console.log(firstNum)
+// operatorButton.forEach((operator) => {
+//     operator.addEventListener('click', () => {
+//         displayPrevious.textContent = displayCurrent.textContent + operator.value
+//         firstNum = displayCurrent.textContent
+//         displayCurrent.textContent = 0
+//         console.log(firstNum)
         
 
-    })
-})
+//     })
+// })
 
-equalsButton.addEventListener('click', () => {
+// equalsButton.addEventListener('click', () => {
     
-})
+// })
 
 clearButton.addEventListener('click', () => {
     displayCurrent.textContent = 0
